57bb0e177f45a9dc820831cf8afc2ed3890c383b
yuval-levin <57755465+yuval-levin@users.noreply.github.com>
2020-10-06 00:37:51 +0300

    password reset email

--- WoltShuffleWeb/Dishuffle/DishuffleApp/settings.py
+++ WoltShuffleWeb/Dishuffle/DishuffleApp/settings.py
@@ -136,7 +136,16 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 #email settings for debug
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'woshuffle@gmail.com'
+EMAIL_HOST_PASSWORD = 'dzfobtclmpxccezs'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'WoltShuffle <noreply@woltshuffle.com>'
+
 
 AUTH_USER_MODEL = 'catalog.Account'
 STATIC_ROOT = ''
